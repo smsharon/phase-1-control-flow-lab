b04db32c2f4277469303cd595a39feb9daf639c7
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-07-11 16:16:16 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result
+  if (ride <= 400) {
+    result = 'This one is on me!';
+}
+else if ( ride > 2000 &&  ride <= 2500){
+  result = 'I will gladly take your thirty bucks.';
+}
+else if (ride > 2500){
+  result = 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+return result;
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let result;
+  result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result;
+}
+
+
+
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default:
+      result = 'Bye.';
+      break;
+  }
+  return result;
 }
